7f1f7bf053b4f223b87ffac507582905ae32c22c
Deyvid333 <rm94999@estudante.fieb.edu.br>
2025-04-04 08:25:51 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,43 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('ronaldo')
+  const [nome, setNome] = useState('tonhão do gás')
 
-  return (
-    <div>
-      <h3>Pizzaria 2D</h3>
+  const Formulário = () => {
+    
+    return (
+      <>
+      <h3>Formulário</h3>
 
       <input
       className="nome"
       onChange={(e)=>{setNome(e. target.value)}}
       type="text" />
-      
     
       <button
       className="botao"
-      onClick={()=>{alert(nome)}}
+      onClick={()=>{
+        alert('o nome digitado foi ' + nome)
+      }
+      }
       >
         
       CLIQUE AQUI
       </button>
+
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <h3>Pizzaria 2D</h3>
+
+      <Formulário />
+      <Formulário />
+      <Formulário />
+      <Formulário />
+      <Formulário />
 
     </div>
   )
