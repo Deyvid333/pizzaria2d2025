e5148dbc6ef9405473a2f0208bf89da4244c5c4f
Deyvid333 <rm94999@estudante.fieb.edu.br>
2025-09-19 09:41:12 -0300

    listagem dinamica

--- src/pages/Produtos/Index.jsx
+++ src/pages/Produtos/Index.jsx
@@ -1,12 +1,22 @@
 const Produtos = () => {
 
+//objeto com a lista de pizzas
+const pizzas = [
+    'Pizza de muçarela',
+    'Pizza de Calabresa',
+    'Pizza de Baiana',
+    'Pizza de Portuguesa'
+]
+
+//interação da lista de pizzas
+const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+
+
     return (
 
         <div>
             <ul>
-                <li>Pizza de muçarela</li>
-                <li>Pizza de calabresa</li>
-                <li>Pizza de baiana</li>
+               {listaPizzas}
             </ul>
         </div>
     )
